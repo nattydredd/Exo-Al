f05711932e04df226d80e2b558f197b2127c5d5f
Nathan Duran <nathan_duran@hotmail.com>
2017-01-24 02:31:42 +0000

    Create site structure
    -Lightcurve TS working
    -Menu buttons highlight selected
    -Added libraries
    -Added empty html/css files
    -Added Controller class

---
+++ src/java/controller/Controller.java
@@ -0,0 +1,94 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@author Nate
+@WebServlet(name = "Controller", urlPatterns = {"/docs/*"})
+public class Controller extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().log("-----------------------------------");
+        getServletContext().log("Controller received a request for (full URI)" + request.getRequestURI());
+
+        //Always Forward to main.jsp
+        String mainPage = "/WEB-INF/docs/main.jsp";
+
+        //Find requested resource
+        String requestPath = request.getRequestURI().substring(request.getContextPath().length());
+
+        getServletContext().log("Controller received a request for " + requestPath);
+
+        //Resource to include defaults to error
+        String include = null;
+        switch (requestPath) {
+
+            case "/docs/home":
+                include = "home.html";
+                break;
+
+            case "/docs/instructions":
+                include = "instructions.html";
+                break;
+
+            case "/docs/classify":
+                include = "classify.html";
+                break;
+
+            case "/docs/results":
+                include = "results.html";
+                break;
+
+        }
+        getServletContext().log("Included resource" + include);
+        request.setAttribute("included", include);
+        
+        request.getRequestDispatcher(mainPage).forward(request, response);
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
